34d1e158fd1ea43b8f97e9655441006affb82e8e
Aluminumm <670729758@qq.com>
2018-06-04 10:19:09 +0800

    seat-handletest

--- server/server-seat-handletest.go
+++ server/server-seat-handletest.go
@@ -23,154 +23,29 @@ import (
 
 func testShowTimeIntervalInfoHandle(t *testing.T) {
 
-	/*
-	defer func() {
-		if err := recover(); err != nil {
-			t.Error(err)
-		}
-	}()
-
-	// 发送http get请求
-	client := &http.Client{}
-	postValues := url.Values{}
-	postValues.Add("school", "sysu")
-	postValues.Add("netID", "15331116")
-	resp, err := client.PostForm("http://localhost:8899/v1/test", postValues)
-	CheckErr(err)
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	CheckErr(err)
-	errresp := ErrorRtnJson{}
-	json.Unmarshal(body, &errresp)
-
-	// 判断返回的错误信息是否符合要求
-	if errresp.Errorcode != 7 || errresp.Errorinformation != "用户当前未登陆" {
-		panic(errors.New("返回错误不正确"))
-	}
-*/
+	
 }
 
 func testShowSeatInfoHandle(t *testing.T) {
 
-	/*
-	defer func() {
-		if err := recover(); err != nil {
-			t.Error(err)
-		}
-	}()
-
-	// 发送http get请求
-	client := &http.Client{}
-	postValues := url.Values{}
-	postValues.Add("school", "sysu")
-	postValues.Add("netID", "15331116")
-	resp, err := client.PostForm("http://localhost:8899/v1/test", postValues)
-	CheckErr(err)
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	CheckErr(err)
-	errresp := ErrorRtnJson{}
-	json.Unmarshal(body, &errresp)
-
-	// 判断返回的错误信息是否符合要求
-	if errresp.Errorcode != 7 || errresp.Errorinformation != "用户当前未登陆" {
-		panic(errors.New("返回错误不正确"))
-	}
-*/
+	
 
 }
 
 func testBookSeatHandle(t *testing.T) {
 
-	/*
-	defer func() {
-		if err := recover(); err != nil {
-			t.Error(err)
-		}
-	}()
-
-	// 发送http get请求
-	client := &http.Client{}
-	postValues := url.Values{}
-	postValues.Add("school", "sysu")
-	postValues.Add("netID", "15331116")
-	resp, err := client.PostForm("http://localhost:8899/v1/test", postValues)
-	CheckErr(err)
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	CheckErr(err)
-	errresp := ErrorRtnJson{}
-	json.Unmarshal(body, &errresp)
-
-	// 判断返回的错误信息是否符合要求
-	if errresp.Errorcode != 7 || errresp.Errorinformation != "用户当前未登陆" {
-		panic(errors.New("返回错误不正确"))
-	}
-*/
+	
 
 }
 
 func testUnbookSeatHandle(t *testing.T) {
 
-	/*
-	defer func() {
-		if err := recover(); err != nil {
-			t.Error(err)
-		}
-	}()
-
-	// 发送http get请求
-	client := &http.Client{}
-	postValues := url.Values{}
-	postValues.Add("school", "sysu")
-	postValues.Add("netID", "15331116")
-	resp, err := client.PostForm("http://localhost:8899/v1/test", postValues)
-	CheckErr(err)
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	CheckErr(err)
-	errresp := ErrorRtnJson{}
-	json.Unmarshal(body, &errresp)
-
-	// 判断返回的错误信息是否符合要求
-	if errresp.Errorcode != 7 || errresp.Errorinformation != "用户当前未登陆" {
-		panic(errors.New("返回错误不正确"))
-	}
-*/
+	
 
 }
 
 func testSigninSeatHandle(t *testing.T) {
 
-	/*
-	defer func() {
-		if err := recover(); err != nil {
-			t.Error(err)
-		}
-	}()
-
-	// 发送http get请求
-	client := &http.Client{}
-	postValues := url.Values{}
-	postValues.Add("school", "sysu")
-	postValues.Add("netID", "15331116")
-	resp, err := client.PostForm("http://localhost:8899/v1/test", postValues)
-	CheckErr(err)
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	CheckErr(err)
-	errresp := ErrorRtnJson{}
-	json.Unmarshal(body, &errresp)
-
-	// 判断返回的错误信息是否符合要求
-	if errresp.Errorcode != 7 || errresp.Errorinformation != "用户当前未登陆" {
-		panic(errors.New("返回错误不正确"))
-	}
-*/
+	
 
 }
